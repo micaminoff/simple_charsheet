5cfcd720391848b9e46b3fc341f5c513d64cd94e
Michael Aminoff <micaminoff@gmail.com>
2020-07-29 15:12:41 +0300

    Basic modal functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Components/Header';
 import AbilityRow from './Components/AbilityRow';
 import Skills from './Components/Skills';
@@ -9,39 +9,51 @@ import { character } from './Types';
 import HealthBox from './Components/HealthBox';
 import CharacterInfo from './Components/CharacterInfo';
 import CombatBox from './Components/CombatBox';
+import EditModal from './Components/EditModal';
 
-const hero: character = data.characters[0];
 
-const App = () => (
-  <div className='App'>
-    <Header name={hero.name} level={hero.level} race={hero.race} dnd_class={hero.dnd_class} />
-    <section className='section character'>
-      <AbilityRow abilities={hero.abilities} />
-      <div className='columns'>
-        <div className='column is-one-quarter'>
-          <Skills proficiencies={hero.proficiencies} proficiency_bonus={hero.proficiency_bonus} abilities={hero.abilities} />
-        </div>
-        <div className='column'>
-          <CombatStats ac={hero.ac} initiative={hero.initiative} speed={hero.speed}/>
-          <HealthBox hp={hero.hp} hit_dice={hero.hit_dice} death_saves={hero.death_saves}/>
-          <CombatBox atk_bonus={hero.atk_bonus} dmg_bonus={hero.dmg_bonus} dmg_die={hero.dmg_die} equipment={hero.equipment}/>
-        </div>
-        <div className='column'>
-          <CharacterInfo
-            inspiration={hero.inspiration}
-            background={hero.background}
-            alignment={hero.alignment}
-            languages={hero.languages}
-            xp={hero.xp}
-            features={hero.features}
-            feats={hero.feats}
-            inventory={hero.inventory}
-          />
-        </div>
-      </div>
 
-    </section>
-  </div>
-);
+const App = () => {
+
+  const [show_modal, set_modal] = useState(false);
+  const hero: character = data.characters[0];
+
+  const toggle_modal = () => (
+    set_modal(!show_modal)
+  );
+
+  return (
+    <div className='App'>
+      {show_modal  ? <EditModal modal_callback={toggle_modal}/> : ''}
+      <Header name={hero.name} level={hero.level} race={hero.race} dnd_class={hero.dnd_class} modal_callback={toggle_modal} />
+      <section className='section character'>
+        <AbilityRow abilities={hero.abilities} />
+        <div className='columns'>
+          <div className='column is-one-quarter'>
+            <Skills proficiencies={hero.proficiencies} proficiency_bonus={hero.proficiency_bonus} abilities={hero.abilities} />
+          </div>
+          <div className='column'>
+            <CombatStats ac={hero.ac} initiative={hero.initiative} speed={hero.speed} />
+            <HealthBox hp={hero.hp} hit_dice={hero.hit_dice} death_saves={hero.death_saves} />
+            <CombatBox atk_bonus={hero.atk_bonus} dmg_bonus={hero.dmg_bonus} dmg_die={hero.dmg_die} equipment={hero.equipment} />
+          </div>
+          <div className='column'>
+            <CharacterInfo
+              inspiration={hero.inspiration}
+              background={hero.background}
+              alignment={hero.alignment}
+              languages={hero.languages}
+              xp={hero.xp}
+              features={hero.features}
+              feats={hero.feats}
+              inventory={hero.inventory}
+            />
+          </div>
+        </div>
+
+      </section>
+    </div>
+  )
+}
 
 export default App;
--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -4,17 +4,23 @@ export interface Props {
     name: string;
     level: number;
     race: string;
-    // silly protected words
     dnd_class: string;
+    modal_callback: () => void;
 }
 
-const Header = ({ name = '', level = 0, race = '', dnd_class = '' }: Props) => (
+const Header = ({ name = '', level = 0, race = '', dnd_class = '', modal_callback }: Props) => (
     <section className='hero is-dark'>
         <div className='hero-body'>
-            <div className='container'>
-                <h1 className='title'>{name}</h1>
-                <h2 className='subtitle'>Level {level} {race} {dnd_class} </h2>
+            <nav className='level'>
+                <div className='level-left container level-item hero-info'>
+                    <h1 className='title'>{name}, 
+                        <span className='subtitle'> Level {level} {race} {dnd_class} </span>
+                    </h1>
+                </div>
+                <div className='level-right button is-success' onClick={modal_callback}>
+                    Edit
             </div>
+            </nav>
         </div>
     </section>
 );
